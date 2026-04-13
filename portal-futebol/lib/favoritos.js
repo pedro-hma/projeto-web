56e285e6c119493ebda151ae82be43b39626ba7e
pedrooayres <pedrohayres@yahoo.com.br>
2026-04-13 13:29:56 -0300

    atualizando com nova versão

--- portal-futebol/lib/favoritos.js
+++ portal-futebol/lib/favoritos.js
@@ -1,30 +1,7 @@
-"use client";
-
-import Parse from "parse/dist/parse.min.js";
-const ParseFixed = Parse.default || Parse;
-
-export const addFavoriteGame = async (game) => {
-  const Favorite = Parse.Object.extend("Favorites");
-  const fav = new Favorite();
-
-  fav.set("name", `${game.strHomeTeam} vs ${game.strAwayTeam}`);
-  fav.set("type", "game");
-
-  await fav.save();
-};
-export const getFavorites = async () => {
-  const query = new Parse.Query("Favorites");
-  const results = await query.find();
-
-  return results.map(item => ({
-    id: item.id,
-    name: item.get("name"),
-    image: item.get("image"),
-  }));
-};
-
-export const deleteFavorite = async (id) => {
+export const updateFavorite = async (id, newName) => {
   const query = new Parse.Query("Favorites");
   const obj = await query.get(id);
-  await obj.destroy();
+
+  obj.set("name", newName);
+  await obj.save();
 };
